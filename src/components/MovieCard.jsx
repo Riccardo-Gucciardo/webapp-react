b78e9674481acc3f3c4293738f52ef1355a68b1d
Riccardo-Guvviardo <gucciardoriccardo003@gmail.com>
2025-03-19 15:37:38 +0100

    chiamate al db

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom"
 
-export default function MovieCard(){
+export default function MovieCard({movie}){
+    const {id,title,director,abstract,image} = movie
+
     return(
         <>
         <div className="card mb-4">
             <div className="card-body">
-                <h5 className="card-title">titolo film</h5>
-                <span>regista</span>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id tempore labore delectus maiores quisquam, facilis soluta laborum, in, dolore earum dignissimos! Doloremque minima et itaque ratione molestias, rem suscipit vitae?</p>
-                <Link to={'movies/2'}>leggi di piu</Link>
+                <h5 className="card-title">{title}</h5>
+                <span>{director}</span>
+                <p>{abstract}</p>
+                <Link to={`movies/${id}`}>leggi di piu</Link>
             </div>
         </div>
         </>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,40 @@
 import MovieCard from "../components/MovieCard"
 // import Review from "../components/Review"
+import axios from "axios"
+import { useState,useEffect } from "react"
 
 export default function HomePage(){
+
+    const [movies,setMovies] = useState([])
+
+    const fetchMovies = () => {
+        axios.get('http://localhost:3000/movies')
+        .then(res => {setMovies(res.data)})
+        .catch(err => {console.log(err);
+        })
+    }
+
+    useEffect( fetchMovies,[] )
+
+    const renderMovies = () => {
+        return(
+            movies.map( (movie) => {
+                return(
+                    <div className="col" key={movie.id}>
+                        <MovieCard movie={movie}/>
+                    </div>
+                )
+            })
+        )
+    }
+
     return(
         <>
         <h1 className="text-primary">Bool books</h1>
         <div className="row row-cols-3">
-            <MovieCard/>
+            {/* <MovieCard/> */}
             {/* <Review/> */}
+            {renderMovies()}
         </div>
         </>
     )
